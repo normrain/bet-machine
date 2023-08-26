5f2fc43c39876f0306a98f175d6781284e4a8d30
Norbert <norbert.amrain@protonmail.com>
2023-08-26 20:55:30 +0300

    Initial commit

---
+++ src/main/java/com/takehometask/BetMachine/BetMachineApplication.java
@@ -0,0 +1,13 @@
+package com.takehometask.BetMachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takehometask/BetMachine/BetMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takehometask.BetMachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
